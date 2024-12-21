366058f2ca086f6d5fea60523ff429682c6656b5
y4edd <yaeyuu0116@icloud.com>
2024-12-22 02:02:48 +0900

    fix: の修正

--- src/app/components/Form.test.tsx
+++ src/app/components/Form.test.tsx
@@ -1,16 +1,15 @@
 // FIXME: テスト用の実装です。削除して開発に着手してください
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Form } from "./Form";
 
-import { fireEvent, render, screen } from '@testing-library/react';
-import { Form } from './Form';
-
-test('名前を表示すること', () => {
+test("名前を表示すること", () => {
   render(<Form name="john" />);
-  expect(screen.getByRole('button')).toBeInTheDocument();
+  expect(screen.getByRole("button")).toBeInTheDocument();
 });
 
-test('ボタンをクリックすると、イベントハンドラーが呼ばれること', () => {
+test("ボタンをクリックすると、イベントハンドラーが呼ばれること", () => {
   const mockFn = jest.fn();
   render(<Form name="john" onSubmit={mockFn} />);
-  fireEvent.click(screen.getByRole('button'));
+  fireEvent.click(screen.getByRole("button"));
   expect(mockFn).toHaveBeenCalled();
 });
