1b49a711cdc1cd1ba9b080b2c99bc6ebd82eca08
y4edd <yaeyuu0116@icloud.com>
2024-12-25 09:28:12 +0900

    fix: css修正

--- src/components/frame/header/Header.tsx
+++ src/components/frame/header/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <WordSearch />
+      <p>RPclothing</p>
+      <Link />
+      </div>
   )
 }
 
