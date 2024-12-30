7e4ce78e227eaaf47267a53a69cbdcd438d79b63
y4edd <yaeyuu0116@icloud.com>
2024-12-30 18:28:50 +0900

    chore: フッターの実装

--- src/components/frame/footer/Footer.tsx
+++ src/components/frame/footer/Footer.tsx
@@ -1,6 +1,10 @@
+import styles from "./Footer.module.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.company}>
+      <p>©株式会社ソニック</p>
+    </div>
   )
 }
 
