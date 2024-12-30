6d1bab4fd3e028a77731d4a9906fa30f88dc517a
y4edd <yaeyuu0116@icloud.com>
2024-12-30 18:44:15 +0900

    chore: ヘッダーのcss修正

--- src/components/frame/header/Header.tsx
+++ src/components/frame/header/Header.tsx
@@ -5,17 +5,21 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <div>
+    <div className={styles.headerContainer}>
+      <div className={styles.container}>
         <WordSearch />
       </div>
       <div className={styles.title}>
         <p>RPclothing</p>
       </div>
-      <Link href="/login" className={styles.login}>ログイン</Link>
-      <button type="button"><SearchIcon sx={{borderColor: "transparent"}}/></button>
+      <div className={styles.navigationContainer}>
+        <Link href="/login" className={styles.login}>ログイン</Link>
+        <button type="button" aria-label="Search">
+          <SearchIcon />
+        </button>
+      </div>  
     </div>
-  )
+  );
 }
 
 export default Header;
--- src/components/frame/header/wordSearch/WordSearch.tsx
+++ src/components/frame/header/wordSearch/WordSearch.tsx
@@ -1,15 +1,23 @@
 import SearchIcon from '@mui/icons-material/Search';
-import styles from "./WordSearch.module.css"
+import styles from "./WordSearch.module.css";
 
 const WordSearch = () => {
   return (
     <>
-      <form method = "get" className={styles.searchForm}>
-        <input type="text" id="search" name="search" placeholder="何かお探しですか？" />
-        <button type="submit"><SearchIcon sx={{borderColor: "transparent"}} /></button>
+      <form method="get" className={styles.searchForm}>
+        <input 
+          type="text" 
+          id="search" 
+          name="search" 
+          placeholder="何かお探しですか？" 
+          aria-label="検索ボックス" 
+        />
+        <button type="submit" aria-label="検索">
+          <SearchIcon />
+        </button>
       </form>
     </>
-  )
+  );
 }
 
 export default WordSearch;
