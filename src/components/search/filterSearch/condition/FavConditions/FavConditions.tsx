3e341b323195016051167d6e91ee6c9c09b660ba
y4edd <yaeyuu0116@icloud.com>
2025-01-08 23:04:17 +0900

    fix: インデントの修正

--- src/components/search/filterSearch/condition/FavConditions/FavConditions.tsx
+++ src/components/search/filterSearch/condition/FavConditions/FavConditions.tsx
@@ -19,25 +19,29 @@ const FavConditions = () => {
   return (
     <>
       <div className={styles.favCondition}>
-      <h3 className={styles.title}>お気に入り条件</h3>
-      <div className={styles.favConditionsContent}>
-        <div className={styles.favConditions}>
-          {/* FIXME:非同期で、お気に入り条件の取得、UI表示を行う処理を実装 */}
-          <Button text={"条件1"} onClick={toSearch} className={buttonStyles.gray} />
-          <Button text={"条件2"} onClick={toSearch} className={buttonStyles.gray} />
-          <Button text={"条件3"} onClick={toSearch} className={buttonStyles.gray} />
-          <Button text={"条件4"} onClick={toSearch} className={buttonStyles.gray} />
+        <h3 className={styles.title}>お気に入り条件</h3>
+        <div className={styles.favConditionsContent}>
+          <div className={styles.favConditions}>
+            {/* FIXME:非同期で、お気に入り条件の取得、UI表示を行う処理を実装 */}
+            <Button text={"条件1"} onClick={toSearch} className={buttonStyles.gray} />
+            <Button text={"条件2"} onClick={toSearch} className={buttonStyles.gray} />
+            <Button text={"条件3"} onClick={toSearch} className={buttonStyles.gray} />
+            <Button text={"条件4"} onClick={toSearch} className={buttonStyles.gray} />
+          </div>
+          <div className={styles.editButton}>
+            <Button
+              text={"お気に入り条件の編集"}
+              onClick={toMyPage}
+              className={buttonStyles.black}
+            />
+          </div>
         </div>
-        <div className={styles.editButton}>
-          <Button text={"お気に入り条件の編集"} onClick={toMyPage} className={buttonStyles.black} />
-        </div>
-      </div>
-      {/* 何も登録されていない場合 */}
-      {/* <p>
-        お気に入り条件は登録されていません<br />
-        マイページから、お気に入り条件を登録しましょう！
-      </p>
-      <Button text={"マイページ"} onClick={toMyPage} className={buttonStyles.black}/> */}
+        {/* 何も登録されていない場合 */}
+        {/* <p>
+          お気に入り条件は登録されていません<br />
+          マイページから、お気に入り条件を登録しましょう！
+        </p>
+        <Button text={"マイページ"} onClick={toMyPage} className={buttonStyles.black}/> */}
       </div>
     </>
   );
