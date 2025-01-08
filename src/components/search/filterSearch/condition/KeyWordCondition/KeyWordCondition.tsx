42d4a4670238c6968a996423fcba56e4e1d3c66a
y4edd <yaeyuu0116@icloud.com>
2025-01-08 15:13:50 +0900

    fix: キーワード検索の実装

--- src/components/search/filterSearch/condition/KeyWordCondition/KeyWordCondition.tsx
+++ src/components/search/filterSearch/condition/KeyWordCondition/KeyWordCondition.tsx
@@ -1,7 +1,10 @@
+import styles from "./KeyWordCondition.module.css"
+
 const KeyWordCondition = () => {
   return (
     <>
       <h3>キーワード</h3>
+      <input type="text" id="キーワード" name="キーワード" className={styles.keyWord} />
     </>
   )
 }
