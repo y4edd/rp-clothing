8949a9ef31c9f20b342ca9a92cf3a117bd8b818d
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2025-01-08 11:17:12 +0900

    fix:Itemコンポーネントのテストの修正

--- src/components/top/Item/Item.test.tsx
+++ src/components/top/Item/Item.test.tsx
@@ -3,7 +3,7 @@ import Item from "./Item";
 
 describe("Itemコンポーネントのテスト", () => {
   test("画像が存在することを確認", () => {
-    render(<Item />);
+    render(<Item linkPath="/" />);
     const element = screen.getByAltText("アイテム画像");
     expect(element).toBeInTheDocument();
   });
