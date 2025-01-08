3491129634657144fc8b57606d9a50248f52e92c
y4edd <yaeyuu0116@icloud.com>
2025-01-08 12:33:22 +0900

    chore: カテゴリ実装

--- src/components/search/filterSearch/condition/CategoryCondition/CategoryCondition.tsx
+++ src/components/search/filterSearch/condition/CategoryCondition/CategoryCondition.tsx
@@ -1,7 +1,35 @@
+import styles from "./CategoryCondition.module.css";
+
 const CategoryCondition = () => {
   return (
     <>
       <h3>カテゴリ</h3>
+      <div className={styles.category}>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="トップス" name="トップス" />
+          <label htmlFor="トップス">トップス</label>
+        </div>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="ズボン・パンツ" name="ズボン・パンツ" />
+          <label htmlFor="ズボン・パンツ">ズボン・パンツ</label>
+        </div>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="スーツ・セットアップ" name="スーツ・セットアップ" />
+          <label htmlFor="スーツ・セットアップ">スーツ・セットアップ</label>
+        </div>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="オーバーオール" name="オーバーオール" />
+          <label htmlFor="オーバーオール">オーバーオール</label>
+        </div>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="レインウェア" name="レインウェア" />
+          <label htmlFor="レインウェア">レインウェア</label>
+        </div>
+        <div className={styles.categoryEach}>
+          <input type="checkbox" id="コート・ジャケット" name="コート・ジャケット" />
+          <label htmlFor="コート・ジャケット">コート・ジャケット</label>
+        </div>
+      </div>
     </>
   )
 }
