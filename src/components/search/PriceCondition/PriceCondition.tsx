1b455bc120d4dea68c6a6255a56280053517f2e0
y4edd <yaeyuu0116@icloud.com>
2025-01-09 18:36:23 +0900

    fix: 不要なtitleクラスの削除

--- src/components/search/PriceCondition/PriceCondition.tsx
+++ src/components/search/PriceCondition/PriceCondition.tsx
@@ -4,7 +4,7 @@ const PriceCondition = () => {
   return (
     <>
       <div className={styles.price}>
-        <h3 className={styles.title}>値段</h3>
+        <h3>値段</h3>
         <div className={styles.priceCondition}>
           <form method="get" className={styles.price}>
             <input type="text" id="text" className={styles.priceEach} />
