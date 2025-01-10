f05ec3c0ca1727834f5734a51c57ed2e6536667b
y4edd <yaeyuu0116@icloud.com>
2025-01-10 11:08:29 +0900

    fix: KeyWordConditionsの不要なフラグメント修正

--- src/components/search/KeyWordCondition/KeyWordCondition.tsx
+++ src/components/search/KeyWordCondition/KeyWordCondition.tsx
@@ -2,12 +2,10 @@ import styles from "./KeyWordCondition.module.css";
 
 const KeyWordCondition = () => {
   return (
-    <>
-      <div className={styles.keyWordCondition}>
-        <h3 className={styles.title}>キーワード</h3>
-        <input type="text" id="キーワード" name="キーワード" className={styles.keyWord} />
-      </div>
-    </>
+    <div className={styles.keyWordCondition}>
+      <h3 className={styles.title}>キーワード</h3>
+      <input type="text" id="キーワード" name="キーワード" className={styles.keyWord} />
+    </div>
   );
 };
 
