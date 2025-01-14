49c233076192b1d71c841fd2e982127258a423d8
y4edd <yaeyuu0116@icloud.com>
2025-01-14 10:48:29 +0900

    feat: 検索条件選択時の遅延問題解決用

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,19 +1,20 @@
 import BreadList from "@/components/frame/breadList/BreadList";
 import SearchModal from "./@modal/page";
 import PageTitle from "@/components/frame/pageTitle/PageTitle";
+import SearchResults from "@/components/search/SearchResults/SearchResults";
 
 const Search = () => {
 
   return (
     <>
-        <BreadList bread={[{ link: "/", title: "トップ" },{ link: "/search", title: "検索結果"}]} />
-        <PageTitle title={"検索結果"} />
+      <BreadList bread={[{ link: "/", title: "トップ" },{ link: "/search", title: "検索結果"}]} />
+      <PageTitle title={"検索結果"} />
 
       {/* クライアント側で検索条件を変更するモーダル */}
       <SearchModal />
 
       {/* 検索結果の表示（サーバー側で描画） */}
-
+      <SearchResults />
     </>
   );
 };
