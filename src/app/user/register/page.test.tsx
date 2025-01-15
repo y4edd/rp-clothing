8f29e5e066dda8c4af8125d4b43d65d9ac8d04ce
前田浩貴 <hiroki-m08@users.noreply.github.com>
2025-01-15 13:34:08 +0900

    fix:build

--- src/app/user/register/page.test.tsx
+++ src/app/user/register/page.test.tsx
@@ -1,14 +1,14 @@
 import { describe } from "node:test";
 import { render, screen } from "@testing-library/react";
-import Registration from "./page";
+import Register from "./page";
 
 jest.mock("@/components/user/Register/RegisterForm", () => {
   return jest.fn(() => <div>Mocked RegistrationForm</div>);
 });
 
-describe("Registrationページのテスト", () => {
+describe("Registerページのテスト", () => {
   test("RegistrationFormコンポーネントがレンダリングされていること", () => {
-    render(<Registration />);
+    render(<Register />);
     expect(screen.getByText("Mocked RegistrationForm")).toBeInTheDocument();
   });
 });
--- src/components/user/register/form/birthday/select/year.test.tsx
+++ src/components/user/register/form/birthday/select/year.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Year from "./Year";
 
 describe("Monthコンポーネントのテスト", () => {
