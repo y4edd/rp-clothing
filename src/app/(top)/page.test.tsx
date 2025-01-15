cd479b369e35e04fd07aba44c05e7380eb600565
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2025-01-15 16:34:09 +0900

    chore:テストの実施

---
+++ src/app/(top)/page.test.tsx
@@ -0,0 +1,27 @@
+import { getNewItems } from "./page";
+
+describe("新着アイテムの取得関数のテスト", () => {
+  const mockItemData = [
+    { itemName: "Item 1", itemCode: "001", imageUrl: "/", itemPrice: 1000 },
+    { itemName: "Item 2", itemCode: "002", imageUrl: "/", itemPrice: 2000 },
+  ];
+  test("正常に商品データを返すとことを確認", async () => {
+    global.fetch = jest.fn().mockReturnValueOnce({
+      ok: true,
+      status: 200,
+      json: () => ({ items: mockItemData }),
+    });
+    const itemData = await getNewItems();
+    // console.log(itemData);
+    expect(itemData).toBe(mockItemData);
+  });
+  test('エラー時にnullを返すことを確認',async()=>{
+    global.fetch = jest.fn().mockReturnValueOnce({
+      ok: false,
+      status: 500,
+    });
+    const itemData = await getNewItems();
+    // console.log(itemData);
+    expect(itemData).toBeNull();
+  })
+});
--- src/components/top/NewItems/NewItems.test.tsx
+++ src/components/top/NewItems/NewItems.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import NewItems from "./NewItems";
 
-describe("NewItemsのテスト", () => {
-  test("新着アイテムが表示されている", () => {
-    render(<NewItems />);
-    const element = screen.getByText("新着アイテム");
-    screen.debug();
-    expect(element).toBeInTheDocument();
+
+jest.mock("../Item/Item", () =>()=> <div>Mocked Item</div>);
+describe("NewItemsコンポーネントのテスト", () => {
+  const mockItemData = [
+    { itemName: "Item 1", itemCode: "001", imageUrl: "/", itemPrice: 1000 },
+    { itemName: "Item 2", itemCode: "002", imageUrl: "/", itemPrice: 2000 },
+  ];
+
+  test("正常にUIが表示されることを確認", () => {
+    render(<NewItems newItems={mockItemData} />);
+    const titleText = screen.getByText("新着アイテム");
+
+    expect(titleText).toBeInTheDocument();
   });
 });
