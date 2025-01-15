67b46bf12bda98b7370a94d439120cd6484778fa
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2025-01-15 10:37:00 +0900

    fix:エラーの修正

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -2,9 +2,7 @@ import LookHistory from "@/components/top/LookHistoryItems/LookHistory";
 import NewItems from "@/components/top/NewItems/NewItems";
 
 const TopPage = async () => {
-  const res =  await fetch('/api/items/searchAll');
-  const data = await res.json()
-  console.log(data)
+
   return (
     <>
       <NewItems />
--- src/components/top/NewItems/NewItems.tsx
+++ src/components/top/NewItems/NewItems.tsx
@@ -1,16 +1,28 @@
 import Item from "../Item/Item";
 import styles from "./NewItems.module.css";
 
-const NewItems = () => {
-  // MEMO アイテム情報取得したら消します
-  const sampleArray = new Array(15).fill("").map((_, index) => index + 1);
+export interface NewItemsModel {
+  itemName: string;
+  itemCode: string;
+  imageUrl: string;
+  itemPrice: number;
+}
 
+const NewItems = async () => {
+  const newItems: NewItemsModel[] |null = await getNewItems();
+  if(!newItems)return <p>データを取得できませんでした。</p>
   return (
     <div className={styles.container}>
       <h2 className={styles.contentTitle}>新着アイテム</h2>
       <div className={styles.gridItems}>
-        {sampleArray.map((item) => (
-          <Item key={item} linkPath="/" />
+        {newItems.map((item) => (
+          <Item
+            key={item.itemCode}
+            itemName={item.itemName}
+            itemCode={item.itemCode}
+            imageUrl={item.imageUrl}
+            itemPrice={item.itemPrice}
+          />
         ))}
       </div>
     </div>
@@ -18,3 +30,19 @@ const NewItems = () => {
 };
 
 export default NewItems;
+
+const getNewItems = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/items/newItems", {
+      next: { revalidate: 3600 },//１時間で再検証
+    });
+    if (!response.ok) {
+      throw new Error("データを取得できませんでした。");
+    }
+    const items = await response.json();
+    return items.items;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
