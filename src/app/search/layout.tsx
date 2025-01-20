e16ff6cd5239ca5cd52f6abebf40f918758f4ded
y4edd <yaeyuu0116@icloud.com>
2025-01-20 11:30:59 +0900

    fix: インデントの修正

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,11 +1,5 @@
-const SearchLayout = ({
-  children,
-}: { children: React.ReactNode }) => {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+const SearchLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default SearchLayout;
