e99d5a679dec264c73fae1e473d14419b5d0c57f
y4edd <yaeyuu0116@icloud.com>
2025-01-21 12:47:51 +0900

    fix: modalコンポーネントのディレクトリを大文字に

--- src/components/search/SearchModal/SearchModal.tsx
+++ src/components/search/SearchModal/SearchModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Modal from "@/components/modal/Modal";
+import Modal from "@/components/Modal/Modal";
 import CategoryCondition from "@/components/search/CategoryCondition/CategoryCondition";
 import FavConditions from "@/components/search/FavConditions/FavConditions";
 import KeyWordCondition from "@/components/search/KeyWordCondition/KeyWordCondition";
