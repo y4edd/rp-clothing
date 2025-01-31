be5a26782d98cafc470b2e7530a570890da2499b
y4edd <yaeyuu0116@icloud.com>
2025-01-31 23:58:54 +0900

    fix: インデントの修正

--- src/components/favorite/RegisterConditionModal/RegisterConditionModal.test.tsx
+++ src/components/favorite/RegisterConditionModal/RegisterConditionModal.test.tsx
@@ -5,10 +5,11 @@ describe("RegisterConditionModalコンポーネントのテスト", () => {
   test("コンポーネントが正常に表示されること", () => {
     const closeModalMock = jest.fn();
     const setIsModalOpenMock = jest.fn();
-    render(<RegisterConditionModal closeModal={closeModalMock} setIsModalOpen={setIsModalOpenMock} />);
+    render(
+      <RegisterConditionModal closeModal={closeModalMock} setIsModalOpen={setIsModalOpenMock} />,
+    );
 
-    const title = screen.getByRole("heading", { name: "お気に入り条件登録"});
+    const title = screen.getByRole("heading", { name: "お気に入り条件登録" });
     expect(title).toBeInTheDocument();
   });
-
-})
+});
