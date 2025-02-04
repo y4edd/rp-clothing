9996882e05bca66d86a18a91a0fb193a18f17683
y4edd <yaeyuu0116@icloud.com>
2025-02-04 16:58:11 +0900

    feat: 検索条件の編集用の非同期処理を実装

--- src/app/api/condition/route.ts
+++ src/app/api/condition/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { search_conditions } from "@/db/schemas/schema";
+import { and, eq } from "drizzle-orm";
 import { type NextRequest, NextResponse } from "next/server";
 
 // 検索条件を登録する
@@ -22,11 +23,34 @@ export const POST = async ( req:NextRequest ) => {
   NextResponse.json({ message: "サーバーエラーが発生しました。" }, { status: 500 });
  } 
 };
+
 // 検索条件を編集する
-
-
-
-
+export const PATCH = async(req: NextRequest) => {
+  // MEMO: ユーザーIDが必要
+  const request = await req.json();
+  const { conditionName, minPrice, maxPrice, selectedCategory, keyWord } = request;
+  try{
+    await db.update(search_conditions)
+      .set({
+        condition_name: conditionName,
+        price_min: minPrice,
+        price_max: maxPrice,
+        category: selectedCategory,
+        word: keyWord
+      })
+      .where(
+        and(
+          // MEMO: ユーザーのID、検索条件のIDを元に書き換える
+          eq(search_conditions.id, 4),  
+          eq(search_conditions.users_id, 1),      
+        )
+      );
+  return NextResponse.json({ message: "検索条件の編集が成功しました。" }, { status: 200 });
+  }catch(error){
+  console.error(error);
+  NextResponse.json({ message: "サーバーエラーが発生しました。" }, { status: 500 });
+  } 
+};
 
 // 検索条件を削除する
 
