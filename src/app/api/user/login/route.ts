7475136fa759fecfca6bf61194198cd50bd39bd4
y4edd <yaeyuu0116@icloud.com>
2025-02-10 14:33:18 +0900

    fix: seedファイルの修正

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -1,27 +1,32 @@
 import { db } from "@/db";
 import { users } from "@/db/schemas/schema";
+import { eq } from "drizzle-orm";
 import { type NextRequest, NextResponse } from "next/server";
+
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
   const { email, password } = body;
+  const bcrypt = require("bcrypt");
+
   try {
     if (!email || !password) {
       NextResponse.json({ message: "未入力です。" }, { status: 400 });
     }
 
-    const selectEmail = await db.selectDistinct(email).from(users);
+    const userData = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
 
-    console.log(selectEmail, "77722");
-
-    if (!selectEmail) console.log(selectEmail, "33");
-
-    if (!selectEmail) {
-      NextResponse.json(
+    if (!userData) {
+      return NextResponse.json(
         { message: "メースアドレスもしくはパスワードが違います。" },
         { status: 409 },
       );
     }
+
+    // パスワードの確認
+    const isMatch = bcrypt.compare(password, userData.password);
 
     // ここにcookieの処理をかく！
 
