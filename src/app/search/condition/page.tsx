13652383efcc16178a16e0b3121b4fc306e045bd
y4edd <yaeyuu0116@icloud.com>
2025-02-20 15:56:13 +0900

    fix: インデント修正

--- src/app/search/condition/page.tsx
+++ src/app/search/condition/page.tsx
@@ -2,8 +2,8 @@ import BreadList from "@/components/frame/breadList/BreadList";
 import PageTitle from "@/components/frame/pageTitle/PageTitle";
 import SearchFilters from "@/components/search/SearchFilters/SearchFilters";
 import SearchModal from "@/components/search/SearchModal/SearchModal";
-import type { SearchParamsProps } from "@/types/search/search";
 import SearchResults from "@/components/search/SearchResults/SearchResults";
+import type { SearchParamsProps } from "@/types/search/search";
 
 const Search = async ({ searchParams }: { searchParams?: Promise<SearchParamsProps> }) => {
   const params = searchParams ? await searchParams : undefined;
