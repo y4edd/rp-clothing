fd5e335dcff2301d5fd1b52b3b9dd38adbbc07c5
y4edd <yaeyuu0116@icloud.com>
2025-02-27 16:27:16 +0900

    feat: idに対する非常識なな型の修正、quantityカラムの追加

--- src/db/migrations/relations.ts
+++ src/db/migrations/relations.ts
@@ -1,61 +1,53 @@
 import { relations } from "drizzle-orm/relations";
-import {
-  cart,
-  favorite_item,
-  favorite_shop,
-  look_history,
-  purchase_history,
-  search_conditions,
-  users,
-} from "../schemas/schema";
+import { users, searchConditions, purchaseHistory, lookHistory, favoriteItem, favoriteShop, cart } from "./schema";
 
-export const cartRelations = relations(cart, ({ one }) => ({
-  user: one(users, {
-    fields: [cart.users_id],
-    references: [users.id],
-  }),
+export const searchConditionsRelations = relations(searchConditions, ({one}) => ({
+	user: one(users, {
+		fields: [searchConditions.usersId],
+		references: [users.id]
+	}),
 }));
 
-export const usersRelations = relations(users, ({ many }) => ({
-  carts: many(cart),
-  favorite_items: many(favorite_item),
-  favorite_shops: many(favorite_shop),
-  look_histories: many(look_history),
-  purchase_histories: many(purchase_history),
-  search_conditions: many(search_conditions),
+export const usersRelations = relations(users, ({many}) => ({
+	searchConditions: many(searchConditions),
+	purchaseHistories: many(purchaseHistory),
+	lookHistories: many(lookHistory),
+	favoriteItems: many(favoriteItem),
+	favoriteShops: many(favoriteShop),
+	carts: many(cart),
 }));
 
-export const favoriteItemRelations = relations(favorite_item, ({ one }) => ({
-  user: one(users, {
-    fields: [favorite_item.users_id],
-    references: [users.id],
-  }),
+export const purchaseHistoryRelations = relations(purchaseHistory, ({one}) => ({
+	user: one(users, {
+		fields: [purchaseHistory.usersId],
+		references: [users.id]
+	}),
 }));
 
-export const favoriteShopRelations = relations(favorite_shop, ({ one }) => ({
-  user: one(users, {
-    fields: [favorite_shop.users_id],
-    references: [users.id],
-  }),
+export const lookHistoryRelations = relations(lookHistory, ({one}) => ({
+	user: one(users, {
+		fields: [lookHistory.usersId],
+		references: [users.id]
+	}),
 }));
 
-export const lookHistoryRelations = relations(look_history, ({ one }) => ({
-  user: one(users, {
-    fields: [look_history.users_id],
-    references: [users.id],
-  }),
+export const favoriteItemRelations = relations(favoriteItem, ({one}) => ({
+	user: one(users, {
+		fields: [favoriteItem.usersId],
+		references: [users.id]
+	}),
 }));
 
-export const purchaseHistoryRelations = relations(purchase_history, ({ one }) => ({
-  user: one(users, {
-    fields: [purchase_history.users_id],
-    references: [users.id],
-  }),
+export const favoriteShopRelations = relations(favoriteShop, ({one}) => ({
+	user: one(users, {
+		fields: [favoriteShop.usersId],
+		references: [users.id]
+	}),
 }));
 
-export const searchConditionsRelations = relations(search_conditions, ({ one }) => ({
-  user: one(users, {
-    fields: [search_conditions.users_id],
-    references: [users.id],
-  }),
+export const cartRelations = relations(cart, ({one}) => ({
+	user: one(users, {
+		fields: [cart.usersId],
+		references: [users.id]
+	}),
 }));
---
+++ src/db/migrations/schema.ts
@@ -0,0 +1,110 @@
+import { pgTable, unique, serial, text, date, timestamp, foreignKey, integer, boolean } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
+
+
+
+export const users = pgTable("users", {
+	id: serial().primaryKey().notNull(),
+	name: text().notNull(),
+	email: text().notNull(),
+	password: text().notNull(),
+	birthday: date().notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	unique("users_email_key").on(table.email),
+]);
+
+export const searchConditions = pgTable("search_conditions", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	conditionName: text("condition_name").notNull(),
+	priceMin: text("price_min"),
+	priceMax: text("price_max"),
+	category: text(),
+	word: text(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "search_conditions_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+	unique("search_conditions_users_id_condition_name_key").on(table.usersId, table.conditionName),
+]);
+
+export const purchaseHistory = pgTable("purchase_history", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	itemPrice: text("item_price").notNull(),
+	itemName: text("item_name").notNull(),
+	itemImage: text("item_image").notNull(),
+	itemShop: text("item_shop").notNull(),
+	date: date().notNull(),
+	isBirthdaySaleUse: boolean("is_birthday_sale_use"),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "purchase_history_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+]);
+
+export const lookHistory = pgTable("look_history", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	itemCode: text("item_code").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "look_history_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+]);
+
+export const favoriteItem = pgTable("favorite_item", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	itemCode: text("item_code").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "favorite_item_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+]);
+
+export const favoriteShop = pgTable("favorite_shop", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	shopCode: text("shop_code").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "favorite_shop_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+]);
+
+export const cart = pgTable("cart", {
+	id: serial().primaryKey().notNull(),
+	usersId: integer("users_id").notNull(),
+	itemCode: text("item_code").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow(),
+}, (table) => [
+	foreignKey({
+			columns: [table.usersId],
+			foreignColumns: [users.id],
+			name: "cart_users_id_fkey"
+		}).onUpdate("cascade").onDelete("cascade"),
+]);
