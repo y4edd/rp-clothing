890178c77d49b82b22f9103f11c27bd7a5094b3c
y4edd <yaeyuu0116@icloud.com>
2025-02-27 11:19:46 +0900

    feat: カートからの削除処理の実装（エラー修正済み）

--- src/components/cart/DeleteCartButton/DeleteCartButton.tsx
+++ src/components/cart/DeleteCartButton/DeleteCartButton.tsx
@@ -1,46 +1,23 @@
-// "use client";
-
-// import Button from "@/components/utils/button/Button";
-// import styles from "./DeleteCartButton.module.css";
-// import { deleteItem } from "@/utils/actions";
-
-// type DeleteCartButtonProps = {
-//   itemCode: string;
-// }
-
-// const DeleteCartButton = ({itemCode}: DeleteCartButtonProps) => {
-//   const handleDelete = async() => {
-//     try{
-//       await deleteItem(itemCode);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-//   return (
-//     <Button type="button" onClick={handleDelete} className={styles.black} text="カートから削除" />
-//   );
-// };
-
-// export default DeleteCartButton;
-
 "use client";
 
 import { deleteItem } from "@/utils/actions";
 import styles from "./DeleteCartButton.module.css";
+import Button from "@/components/utils/button/Button";
 
 type DeleteCartButtonProps = {
   itemCode: string;
   userId: string;
 };
 
-const DeleteCartButton = ({ itemCode,userId }: DeleteCartButtonProps) => {
-  const value = { itemCode,userId };
-  const valueString = JSON.stringify(value);
+const DeleteCartButton = ({ itemCode, userId }: DeleteCartButtonProps) => {
+
+  const handleDelete = async() => {
+    const response = await deleteItem(itemCode, userId);
+    console.log(response);
+  };
+
   return (
-    <form action={deleteItem}>
-      <input type="hidden" name="deleteItem" value={valueString} />
-      <button type="submit" className={styles.black}>カートから削除</button>
-    </form>
+    <Button type="submit" className={styles.black} onClick={handleDelete} text={"カートから削除"} />
   );
 };
 
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -5,15 +5,11 @@ import { cart } from "@/db/schemas/schema";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
-export const deleteItem = async (formData: FormData) => {
-  const dataObj = formData.get("deleteItem") as string;
-  // 「kaucowking:10056715」が入ってくるのを留意しないとダメ
-  const { itemCode, userId } = JSON.parse(dataObj);
+// カートから商品を削除する関数
+export const deleteItem = async (itemCode: string, userId: string) => {
 
   try {
-    // const res =await db.delete(cart).where(and(eq(cart.item_code, itemCode),eq(cart.users_id, userId)));
-    const res =await db.select().from(cart).where(and(eq(cart.item_code, itemCode),eq(cart.users_id, userId)));
-    console.log(res);
+    await db.delete(cart).where(and(eq(cart.item_code, itemCode),eq(cart.users_id, userId)));
     revalidatePath("/");
   } catch (error) {
     console.error("削除エラー:", error);
