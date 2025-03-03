bb18bb71aaffaad3cc88bdcd31c8e3167ba30d90
y4edd <yaeyuu0116@icloud.com>
2025-03-03 15:59:58 +0900

    feat: 非ログ時、カートにすでにある商品について、再度追加があった時に処理を実装

--- src/app/api/cart_items/route.ts
+++ src/app/api/cart_items/route.ts
@@ -106,40 +106,66 @@ export const POST = async (request: NextRequest) => {
   const req = await request.json();
   const userIdString = await checkAuth();
   const userId = Number(userIdString);
+  const itemCode = req.itemCode;
+  const decodedItemCode = decodeURIComponent(itemCode).replace(/^"|"$/g, "");
+  const selectedQquantity = req.selectedQuantity;
+  let cartItems:any[] = [];
+
   try {
-    const itemCode = req.itemCode;
-    const decodedItemCode = decodeURIComponent(itemCode).replace(/^"|"$/g, "");
-    const selectedQquantity = req.selectedQuantity;
-    let cartItems = [];
 
-    // 非ログであれば、一度だけredisとcookieに、ランダム生成された同一のsessionIdを渡す
-    if (!userId) {
-      // もしsessionIdがあれば同一のsessionIdをキーとしさらにカートアイテムを追加
-      // クッキーから既存の sessionId を取得
+    if(userId) {
+      // カートテーブルから商品を取得し、なければ新たに追加
+      const cartItem = await db
+      .select()
+      .from(cart)
+      .where(and(eq(cart.users_id, userId), eq(cart.item_code, decodedItemCode)));
+
+      if (cartItem.length === 0) {
+        await db
+          .insert(cart)
+          .values({ users_id: userId, item_code: decodedItemCode, quantity: selectedQquantity });
+        return NextResponse.json({ message: "商品をカートに追加しました。" }, { status: 200 });
+      } else {
+        await db
+          .update(cart)
+          .set({ quantity: cartItem[0].quantity + selectedQquantity })
+          .where(and(eq(cart.users_id, userId), eq(cart.item_code, decodedItemCode)));
+        return NextResponse.json({ message: "商品をカートに追加しました。" }, { status: 200 });
+      }
+    } else {
+      // 非ログの状態の場合、sessionIdの有無を確認する
+      // すでに商品がカート内にある場合、クッキーから既存の sessionId を取得
       const existingSessionId = request.cookies.get("sessionId")?.value;
+      if(existingSessionId) {
+        const cartData = await redisClient.get(`sessionId:${existingSessionId}`);
+        cartItems = cartData ? JSON.parse(cartData) : [];
+        const existingItem = cartItems.find((item) =>
+          item.cartItem === itemCode
+        );
+        console.log(existingItem);
 
-      // 既存のカート情報を取得（空の場合は [] をデフォルト）
-      const cartData = await redisClient.get(`sessionId:${existingSessionId}`);
-      cartItems = cartData ? JSON.parse(cartData) : [];
+        // すでにカート内にある商品か確認
+        if(existingItem) {
+          cartItems = cartItems.map((item) => {
+            return item.cartItem === itemCode ? { ...item, quantity: item.quantity + selectedQquantity }
+            : item
+          });
+        } else {
+          // カート内にない商品の追加なら、配列に追加
+          cartItems.push({ cartItem: itemCode, quantity: selectedQquantity });
+        }
 
-      // 新しい商品を追加
-      cartItems.push({ cartItem: itemCode, quantity: selectedQquantity });
-
-      // Redis に更新（リスト全体を保存）
-      await redisClient.set(
-        `sessionId:${existingSessionId}`,
-        JSON.stringify(cartItems),
-        "EX",
-        REDIS_MAX_AGE,
-      );
-
+        // Redis に更新（リスト全体を保存）
+        await redisClient.set(
+          `sessionId:${existingSessionId}`,
+          JSON.stringify(cartItems),
+          "EX",
+          REDIS_MAX_AGE,
+        );
+      } else {
       // sessionIdなければredisには「sessionId：hogehoge」をキーとし、
       // 「cart:fugafuga」みたいな感じでポストする
-      if(!existingSessionId) {
         const sessionId = uuidv4();
-        cartItems = cartData ? JSON.parse(cartData) : [];
-
-        // 新しい商品を追加
         cartItems.push({ cartItem: itemCode, quantity: selectedQquantity });
 
         await redisClient.set(
@@ -154,25 +180,6 @@ export const POST = async (request: NextRequest) => {
       }
       return NextResponse.json({ message: "商品をカートに追加しました。" }, { status: 200 });
     };
-
-    // カートテーブルから商品を取得し、なければ新たに追加
-    const cartItem = await db
-      .select()
-      .from(cart)
-      .where(and(eq(cart.users_id, userId), eq(cart.item_code, decodedItemCode)));
-
-    if (cartItem.length === 0) {
-      await db
-        .insert(cart)
-        .values({ users_id: userId, item_code: decodedItemCode, quantity: selectedQquantity });
-      return NextResponse.json({ message: "商品をカートに追加しました。" }, { status: 200 });
-    } else {
-      await db
-        .update(cart)
-        .set({ quantity: cartItem[0].quantity + selectedQquantity })
-        .where(and(eq(cart.users_id, userId), eq(cart.item_code, decodedItemCode)));
-      return NextResponse.json({ message: "商品をカートに追加しました。" }, { status: 200 });
-    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "商品をカートに追加できませんでした。" }, { status: 400 });
--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -9,16 +9,14 @@ export const REDIS_MAX_AGE = 60 * 60 * 48;
 export const removeCartItem = async(sessionId:any,targetCartItem:any) => {
   const data = await redisClient.get(`sessionId:${sessionId}`);
   if(!data) {
-    console.log("データなし");
+    console.error("データなし");
     return;
   }
   const dataJSON:CartItemInRedis = JSON.parse(data);
   const deletedCartItem = dataJSON.filter(
     item => decodeURIComponent(item.cartItem) !== targetCartItem
   );
-  console.log(deletedCartItem);
   const deletedCartItemJSON = JSON.stringify(deletedCartItem);
-
 
   await redisClient.set(`sessionId:${sessionId}`, deletedCartItemJSON);
   // cacheを削除し再レンダリング
