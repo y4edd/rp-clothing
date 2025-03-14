bb39a856cf569b1ad1c307a08f364b65d5e41033
y4edd <yaeyuu0116@icloud.com>
2025-03-14 17:44:13 +0900

    fix: インデントの修正

--- src/app/api/cart_items/route.ts
+++ src/app/api/cart_items/route.ts
@@ -28,10 +28,9 @@ export const GET = async (request: NextRequest) => {
     const cartItems = await db
       .select({ itemCode: cart.itemCode, quantity: cart.quantity })
       .from(cart)
-      .where(eq(cart.usersId, userId))
-    ;
+      .where(eq(cart.usersId, userId));
 
-    if(cartItems.length === 0) {
+    if (cartItems.length === 0) {
       return NextResponse.json({ message: "カート内に商品はありませんでした。" }, { status: 200 });
     }
 
