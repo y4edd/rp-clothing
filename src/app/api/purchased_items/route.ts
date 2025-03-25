cbd62341afd7920a9649cff5f078b0220bb652cc
yuta-toida <yuta.tennis.0418@gmail.com>
2025-03-25 13:43:43 +0900

    API作成途中

--- src/app/api/purchased_items/route.ts
+++ src/app/api/purchased_items/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import { purchase_history } from "@/db/schemas/schema";
 import { checkAuth } from "@/utils/checkAuth";
+import axios from "axios";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -17,13 +18,30 @@ export const GET = async (req: NextRequest) => {
       return NextResponse.json({ message: "ユーザーIDの取得処理に失敗しました"})
     }
 
-    const purchasedHistory = await db
+    const purchasedHistories = await db
       .select()
       .from(purchase_history)
       .where(eq(purchase_history.users_id, userId))
       .orderBy(purchase_history.date)
       .limit(5);
 
+    const itemDetails = await Promise.all(
+      purchasedHistories.map(async (purchaseHistory,index) => {
+        try {
+          const response = await axios.get(
+            "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601",
+            {
+              params: {
+                applicationId: process.env.RAKUTEN_API_ID,
+                itemCode: purchaseHistory.
+              }
+            }
+          )
+        } catch (error) {
+          
+        }
+      })
+    )
   } catch (error) {
     
   }
